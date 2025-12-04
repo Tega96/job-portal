bd60ad569fcc680bdf1a1c3ac0eeac0345a70d54
Otega Otite <otiteotega@gmail.com>
2025-12-04 06:55:47 +0100

    validate Password and Avatar in helper function

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -5,3 +5,30 @@ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 if (!emailRegex.text(email)) return 'Please enter a valid email address';
 return '';
 };
+
+export const validatePassword = (password) => {
+    if (!password) return "Password is required";
+    if (password.length < 8) return "Password must be at least 8 characters"
+    if (!/(?=.*[a-z]))/.test(password))
+        return "Password must contain at least one lowercase letter";
+    if (!/(?=.*[A-Z]))/.test(password))
+        return "Password must contain at least one uppercase letter";
+    if (!/(?=.*\d))/.test(password))
+        return "Password must contain at least one number";
+    return "";
+}
+
+export const validateAvatar = (file) => {
+    if (!file) return ""; // Avatar is optional
+
+    const allowedTypes = ["image/jpeg", "image/jpg", "image/png"];
+    if (!allowedTypes.includes(file.type)) {
+        return "Avatar must be a JPG or PNG file";
+    }
+
+    const maxSize = 5 * 1024 * 1024; //5mb
+    if (file.size > maxSize) {
+        return "Avatar must be less than 5MB"
+    }
+    return "";
+}
