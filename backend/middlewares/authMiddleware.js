ac818aa96f2a7dbe7f89983ce009c984cc9ba939
Otega Otite <otiteotega@gmail.com>
2025-12-04 15:19:55 +0100

    develop the auth including routes, middleware and controllers

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/User'
+
+// Middleware to protect routes
+const protect = async (req, res, next) => {
+    try {
+        let token = req.headers.authorization;
+
+        if (token && token.startsWith("Bearer")) {
+            token = token.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select("password");
+            next();
+        } else {
+            res.status(401).json({ message: "Not authorized, no token"});
+        }
+    } catch (err) {
+        res.status(401).json({ message: "Token failed", error: err.message })
+    }
+};
+
+export default protect;
