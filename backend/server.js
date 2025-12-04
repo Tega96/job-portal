d45e1c8c184616ed2c5a335f6ffe29e10f4aca56
Otega Otite <otiteotega@gmail.com>
2025-12-04 11:33:20 +0100

    connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,42 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import path from 'path'
+import connectDB from './config/db.js'
+// import authRoutes from "./routes/authRoutes"
 
+dotenv.config();
 
 const app = express() 
-const PORT = 3300
+const PORT = process.env.PORT || 3300
+
+// Midleware to handle CORS
+app.use(
+    cors({
+        origin: process.env.ALLOWED_ORIGIN || `http://localhost:${PORT}`,
+        methods: ['GET', "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+// Connect to Database
+connectDB();
+
+// Middleware
+app.use(express.json)
+// app.use(express.urlencoded({extended: true}))
+
+// Routes
+// app.use("/api/auth", authRoutes)
+
+// Serve uploads folder
+// app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`)
 })
