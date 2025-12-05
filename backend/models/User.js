dedea9024594cdb2b3dd674ae5e965745c3fc8d6
Otega Otite <otiteotega@gmail.com>
2025-12-05 22:48:09 +0100

    Implement the Application business logic for the various application routes.

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    role: {type: String, enum: ["jobseekder", 'employer'], required: true},
+    role: {type: String, enum: ["jobseeker", 'employer'], required: true},
     avatar: String,
     resume: String,
     // for employer
